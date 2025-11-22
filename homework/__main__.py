71177835c4a2e6c2b91efcb03dbfe9494c0bb2f9
kcastanoa <kcastanoa@unal.edu.co>
2025-11-22 11:30:06 -0500

    Taller 2

---
+++ homework/__main__.py
@@ -0,0 +1,6 @@
+# py -m homework data/input data/output
+
+from homework.src.wordcount import main
+
+if __name__ == "__main__":
+    main()
--- tests/test_homework.py
+++ tests/test_homework.py
@@ -21,7 +21,7 @@ def test_homework():
 
     try:
         subprocess.run(
-            ["python3", "-m", "homework", "data/input", "data/output"],
+            ["python 3", "-m", "homework", "data/input", "data/output"],
             check=True,
         )
     except subprocess.CalledProcessError as e:
